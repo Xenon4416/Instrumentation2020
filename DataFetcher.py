b6769e57bd69fabcf66ac1a341583335fe71d812
Xenon4416 <xenonshrestha4416@gmail.com>
2020-01-29 18:33:04 +0545

    finished base project

--- DataFetcher.py
+++ DataFetcher.py
@@ -1,12 +1,22 @@
 import serial
 import time
+from datetime import datetime
+
 
 try:
-    arduino = serial.Serial("COM6",timeout=1)
+    ard = serial.Serial("COM6", 9600, timeout=1)
 except:
     print('Please check the port')
 
+file = open('Dat.txt', 'a') # TODO: Edit Dat.txt to Data.txt
 while True:
-    temp = str(arduino.readline())
-    print(temp[2:-5])
+    temp = str(ard.readline())
+    if temp != "b''":
+        temp = temp.split('\\')[0][2:]
+        now = datetime.now().strftime("%m/%d/%Y %I:%M:%S %p")
+        if temp == 'Entry':
+            now += ' 1'
+        elif temp == 'Exit':
+            now += ' 0'
+        print(now, file=file)
     time.sleep(0.1)
